2c4e491d0d16689ecdb9515227cb55465e392127
Diego Fernández Fernández <diego@simplelogica.net>
2010-04-17 19:03:04 +0200

    Más configuración para testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,7 +22,11 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
+config.gem 'rspec'
+config.gem 'rspec-rails'
 config.gem "steak"
+config.gem "shoulda"
+
 # Use SQL instead of Active Record's schema dumper when creating the test database.
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
