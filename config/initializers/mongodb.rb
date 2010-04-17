86bba3ecf3a29b5a058b4360f648f1b19ba2a3b1
Diego Fernández Fernández <diego@simplelogica.net>
2010-04-17 19:04:15 +0200

    Versión mínima para la config de Mogo, menos es más y tal

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,30 +1,15 @@
 include MongoMapper
 
-db_config = YAML::load(File.read(File.join(Rails.root, "/config/database.yml")))
-
-if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
-  mongo = db_config[Rails.env]
-  MongoMapper.connection = Mongo::Connection.new(mongo['host'] || 'localhost',
-                                                 mongo['port'] || 27017,
-                                                :logger => Rails.logger)
-  MongoMapper.database = mongo['database']
-  
-  # if mongo['username'] && mongo['password']
-  #     MongoMapper.database.authenticate(mongo['username'], mongo['password'])
-  #   end
-end
-
-ActionController::Base.rescue_responses['MongoMapper::DocumentNotFound'] = :not_found
-
-# Used for image uploads
-# CarrierWave.configure do |config|
-#   mongo = db_config[Rails.env]
-#   config.grid_fs_database = mongo['database']
-#   config.grid_fs_host = mongo['host'] || 'localhost'
-#   config.grid_fs_access_url = "gridfs"
-#   config.grid_fs_username = mongo['username']
-#   config.grid_fs_password =  mongo['password']
+# Settings for MongoHQ
+# if ['production', 'development'].include?(Rails.env)
+#  MongoMapper.connection = Mongo::Connection.new('db.mongohq.com')
+#  MongoMapper.database = 'brooklyn'
+#  MongoMapper.database.authenticate('kyle', 'secret')
+# else
+#  MongoMapper.database = "news-#{Rails.env}"
 # end
+
+MongoMapper.database = "prolix-#{Rails.env}"
 
 # It's also possible to define indexes in the the model itself; however,
 # a few issues are being worked out still. This is a temporary solution.
@@ -32,8 +17,8 @@ ActionController::Base.rescue_responses['MongoMapper::DocumentNotFound'] = :not_
 # MongoMapper.ensure_indexes!
 
 # Handle passenger forking.
-# if defined?(PhusionPassenger)
-#    PhusionPassenger.on_event(:starting_worker_process) do |forked|
-#      MongoMapper.database.connect_to_master if forked
-#    end
-# end
+if defined?(PhusionPassenger)
+   PhusionPassenger.on_event(:starting_worker_process) do |forked|
+     MongoMapper.database.connect_to_master if forked
+   end
+end
