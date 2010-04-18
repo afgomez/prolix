2fa226abbe0f38dc3764b9fbfacc6983d263d622
Alejandro Fernández <antarticonorte@gmail.com>
2010-04-18 17:34:45 +0200

    Listado de ciudades y buscador

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,13 @@
 class CitiesController < ApplicationController
+  
+  
+  def index
+    # Datos de prueba para la maqueta...
+    cities = %w{Alicante Valencia Zaragoza León Pamplona Cáceres Badajoz Oviedo Teruel Barcelona}.sort
+    @query = params[:query].downcase
+    @cities = @query.nil? ? cities : cities.map { |city| city if city.downcase.include? @query }.compact
+  end
+
   def show
     @city = ''
   end
